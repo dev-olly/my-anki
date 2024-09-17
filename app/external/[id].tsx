cf84a56c16c0b7a93451744c1d9ea085dfd1610e
Emmanuel Onyebueke <developer.olly@gmail.com>
2024-09-17 09:46:42 +0200

    feat: deck meta information

--- app/external/[id].tsx
+++ app/external/[id].tsx
@@ -1,10 +1,22 @@
+import { GrayThemedButton } from "@/components/ThemedButton";
 import { View, Text, Image, StyleSheet } from "react-native";
 
 export default function ExternalDeckScreen() {
   return (
     <View>
       <Image source={{uri: 'https://res.cloudinary.com/db5aqdx6s/image/upload/v1725405050/nicos_weg_cr1lj4.webp'}} style={styles.deckItemImage} />
-      <Text>External Deck</Text>
+      <View style={{padding: 16}}>
+        <Text style={{fontSize: 20, fontWeight: 'bold'}}>Nicos weg - Gerechtigkeit?</Text>
+        <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginTop: 8}}>
+          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <Text style={{fontSize: 12, fontWeight: 'bold'}}> A1</Text>
+            <Text style={{marginLeft: 10, fontSize: 12}}>|   100 words</Text>
+          </View>
+          <GrayThemedButton onPress={() => {}} extraStyle={{marginTop: 0, width: 100, padding: 8, height: 30}}>
+            <Text style={{fontSize: 12, fontWeight: 'bold'}}>Add to deck</Text>
+          </GrayThemedButton>
+        </View>
+      </View>
     </View>
   )
 }
