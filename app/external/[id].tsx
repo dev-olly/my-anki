fa39a0eb4e74f568ffc8a0e23a44cb81f5090898
Emmanuel Onyebueke <developer.olly@gmail.com>
2024-09-17 22:13:16 +0200

    feat: render details

--- app/external/[id].tsx
+++ app/external/[id].tsx
@@ -1,36 +1,49 @@
 import { GrayThemedButton, PrimaryThemedButton } from "@/components/ThemedButton";
-import { View, Text, Image, StyleSheet } from "react-native";
+import { useFetchDecks } from "@/hooks/useFetchDecks";
+import { useLocalSearchParams } from "expo-router";
+import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
 
 export default function ExternalDeckScreen() {
+  const {id} = useLocalSearchParams();
+  const {decks} = useFetchDecks();
+  const deck = decks.find((deck) => deck.id === id);
+
+  if(!deck) {
+    return <Text>Oops not found!</Text>
+  }
   return (
-    <View>
-      <Image source={{uri: 'https://res.cloudinary.com/db5aqdx6s/image/upload/v1725405050/nicos_weg_cr1lj4.webp'}} style={styles.deckItemImage} />
-      <View style={{padding: 16}}>
-        <Text style={{fontSize: 20, fontWeight: 'bold'}}>Nicos weg - Gerechtigkeit?</Text>
-        <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginTop: 8}}>
-          <View style={{flexDirection: 'row', alignItems: 'center'}}>
-            <Text style={{fontSize: 12, fontWeight: 'bold'}}> A1</Text>
-            <Text style={{marginLeft: 10, fontSize: 12}}>|   100 words</Text>
-          </View>
-          <GrayThemedButton onPress={() => {}} extraStyle={{marginTop: 0, width: 100, padding: 8, height: 30}}>
-            <Text style={{fontSize: 12, fontWeight: 'bold'}}>Add to deck</Text>
-          </GrayThemedButton>
-        </View>
-      </View>
-
-      <View style={{paddingHorizontal: 16, marginTop: 8}}>
-        <View style={{backgroundColor: 'white', padding: 12, borderRadius: 10}}>
-          <Text style={{fontSize: 14, fontWeight: 'bold'}}>Der Mann</Text>
-          <Text style={{fontSize: 12, color: 'gray', marginTop: 4}}>The Man</Text>
-        </View>
-      </View>
-
+    <ScrollView>
       <View>
-        <PrimaryThemedButton onPress={() => {}}>
-          <Text style={{fontSize: 14, fontWeight: 'bold', color: 'white'}}>Add to deck</Text>
-        </PrimaryThemedButton>
+        <Image source={{uri: 'https://res.cloudinary.com/db5aqdx6s/image/upload/v1725405050/nicos_weg_cr1lj4.webp'}} style={styles.deckItemImage} />
+        <View style={{padding: 16}}>
+          <Text style={{fontSize: 20, fontWeight: 'bold'}}>Nicos weg - {deck.title}</Text>
+          <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginTop: 8}}>
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <Text style={{fontSize: 12, fontWeight: 'bold'}}> {deck.level}</Text>
+              <Text style={{marginLeft: 10, fontSize: 12}}>|   {deck.words.length} words</Text>
+            </View>
+            <GrayThemedButton onPress={() => {}} extraStyle={{marginTop: 0, width: 100, padding: 8, height: 30}}>
+              <Text style={{fontSize: 12, fontWeight: 'bold'}}>Add to deck</Text>
+            </GrayThemedButton>
+          </View>
+        </View>
+
+        <View style={{paddingHorizontal: 16, marginTop: 8}}>
+          {deck.words.map((word, index) => (
+            <View key={index} style={{backgroundColor: 'white', padding: 12, borderRadius: 10, marginBottom: 8}}>
+              <Text style={{fontSize: 14, fontWeight: 'bold'}}>{word.german}</Text>
+              <Text style={{fontSize: 12, color: 'gray', marginTop: 4}}>{word.translation}</Text>
+            </View>
+          ))}
+        </View>
+
+        <View>
+          <PrimaryThemedButton onPress={() => {}}>
+            <Text style={{fontSize: 14, fontWeight: 'bold', color: 'white'}}>Add to deck</Text>
+          </PrimaryThemedButton>
+        </View>
       </View>
-    </View>
+    </ScrollView>
   )
 }
 
